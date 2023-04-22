e5d8d1915fde9903d74eefc23fc27304003887ee
reggy0 <85451025+reggy0@users.noreply.github.com>
2023-04-22 03:13:19 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import re
-from flask import Flask, jsonify, request,render_template
+from flask import Flask, jsonify, request
 from happytransformer import HappyGeneration, GENSettings
 
-app = Flask(__name__,template_folder='Tamplate')
+app = Flask(__name__)
 
 
-happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-2.7B")
+happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-1.3B")
 
 training_cases = """Keywords:Self Programmable,medical monitoring device,Uploads algorithms, applications,outside computers,Wireless Communication
 Output:A network system managing an on-demand service within a geographic region can receive, over a network, multi-user request data corresponding to a request for service for a plurality of users. The request data can indicate one or more start locations, a plurality of users, and one or more service locations. In response to receiving the request data, the network system can select a set of service providers from a plurality of candidate service providers in the geographic region to provide the requested service. The service providers can be selected based on optimizations of one or more service parameters including estimated fares for the plurality of users, ETAs to the start location, ETAs to the service locations, etc. The network system can further determine routes for the set of service providers from their respective locations to the start or service location(s) and from the start or 
@@ -32,17 +32,12 @@ Keywords:System and Method,Improving remote patient monitoring,Using plurality o
 Output:A wireless devices that include a virtual assistant avatar, which is capable of communicating with patients in a natural and intuitive manner. The system also includes a physician web portal, which allows healthcare professionals to remotely monitor patient data and provide feedback and recommendations. The virtual assistant avatar provides patients with an easy and interactive way to report symptoms and respond to questions, while the physician web portal allows doctors to remotely test patients and monitor their progress. The system uses artificial intelligence to analyze patient data and provide insights and recommendations to healthcare professionals, while also utilizing speech to text, text to speech, and natural language processing to ensure effective communication between patients and the virtual assistant avatar. Overall, the system and method is designed to improve remote patient monitoring, provide more efficient and effective communication between patients and healthcare professionals, and facilitate better overall patient outcomes.
 ###"""
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route("/generate", methods=["POST"])
-def generate_text():
-    # Get input data from request
-    data = request.json
+# API logic
+@app.route('/api/get_data', methods=['POST'])
+def get_data():
+    data = request.get_json() 
     keywords = [keyword.strip() for keyword in data["input_text"].split(",")]
     
- 
     #Creating the prompt
     def create_prompt(training_cases, keywords):
         keywords = ", ".join(keywords)
